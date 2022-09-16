0e85a482c390f998bcb4a8500f3e4bb1ae8e5be7
KOMa <alekseyiv89@mail.ru>
2022-09-16 11:25:36 +0300

    Initial commit

---
+++ src/main/java/web/model/User.java
@@ -0,0 +1,16 @@
+package web.model;
+
+public class User {
+    private long id;
+
+    private String name;
+
+    private String surname;
+
+    private int age;
+
+    private String city;
+
+    public User() {
+    }
+}
